7d629441ce595df930094e0defd73bc6e918f8d8
Mahak <mahak@rightster.com>
2014-01-29 18:00:28 +0530

    Update Constants.java

--- MARS/Reusables/Constants.java
+++ MARS/Reusables/Constants.java
@@ -6,18 +6,12 @@ import org.openqa.selenium.WebElement;
 
 public class Constants{
 	public static final String TestDataFileLocation = "C:\\Users\\rightster\\Documents\\MARSTestInput1405.xls";
-	public static final String FuncTest_URL = "https://176.34.240.152";
+	public static final String FuncTest_URL = "";
 	public static final String LogInScreen_URL = "/user/login";
 	private static final String SIGN_OUT_XPATH = "html/body/div[1]/div/header/div/div/div[3]/a";
-	public static final String PAGE_TITLE_EPG_SCREEN = "Programme Guide - Live Sports - Rightster";
+	public static final String PAGE_TITLE_EPG_SCREEN = "P";
 	
-	//country Xpaths to be used when setting restrictions
-	public static final String AFRICA_DROP_DOWN_XPATH = "html/body/div[1]/div/div[2]/div[1]/form/div[2]/h3[1]/a";
-	public static final String ASIA_DROP_DOWN_XPATH = "html/body/div[1]/div/div[2]/div[1]/form/div[2]/h3[3]/a";	
-	public static final String EUROPE_DROP_DOWN_XPATH = "html/body/div[1]/div/div[2]/div[1]/form/div[2]/h3[7]";
-	public static final String NORTHAMERICA_DROP_DOWN_XPATH = "html/body/div[1]/div/div[2]/div[1]/form/div[2]/h3[11]/a";
-	public static final String OCEANIA_DROP_DOWN_XPATH = "html/body/div[1]/div/div[2]/div[1]/form/div[2]/h3[12]/a";
-	public static final String SOUTHAMERICA_DROP_DOWN_XPATH = "html/body/div[1]/div/div[2]/div[1]/form/div[2]/h3[14]/a";
+
 	
 	Base base = new Base();
 	
